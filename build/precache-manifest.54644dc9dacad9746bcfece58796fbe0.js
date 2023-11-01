e26fbddd59d2860b3a20f896091834a31041e9db
AmanPachauria <98804864+AmanPachauria@users.noreply.github.com>
2023-11-01 19:45:30 +0530

    done

---
+++ build/precache-manifest.54644dc9dacad9746bcfece58796fbe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb92218a18d60522feb411dc676f0a8",
+    "url": "/VisualSortX/index.html"
+  },
+  {
+    "revision": "178493b7e5c22bea284b",
+    "url": "/VisualSortX/static/css/main.f56a9537.chunk.css"
+  },
+  {
+    "revision": "ae4e3fa89f350a97ec16",
+    "url": "/VisualSortX/static/js/2.6d8871a7.chunk.js"
+  },
+  {
+    "revision": "178493b7e5c22bea284b",
+    "url": "/VisualSortX/static/js/main.eccbc9c5.chunk.js"
+  },
+  {
+    "revision": "3453df586fd700bf0b5e",
+    "url": "/VisualSortX/static/js/runtime-main.9e94ba92.js"
+  }
+]);
